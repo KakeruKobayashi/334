6c2f987c1fa7716adbc3d1c7c37a3a25c6665f83
Naohiro Murata <7419110@ed.tus.ac.jp>
2023-08-08 15:01:09 +0900

    service作成

--- src/dao/UserCourseDAO.java
+++ src/dao/UserCourseDAO.java
@@ -2,12 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import model.Course;
 
 public class UserCourseDAO {
 	private Connection connection;
@@ -41,11 +36,8 @@ public class UserCourseDAO {
 
 				}
 			} catch (SQLException e) {
-
 				throw new RuntimeException("SQLの実行の後処理に失敗しました", e);
-
 			}
 		}
-	}
 	}
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,5 +1,40 @@
 package service;
 
-public class UserService {
+import java.sql.Connection;
 
+import dao.UserDAO;
+import db.ConnectionManager;
+import model.User;
+
+public class UserService {
+	public int insertUser(User user) {
+		ConnectionManager connectionManager = new ConnectionManager();
+		try {
+			Connection connection = connectionManager.getConnection();
+			UserDAO userDAO = new UserDAO(connection);
+			int result = userDAO.insertUser(user);
+			connectionManager.commit();
+			return result;
+		} catch (RuntimeException e) {
+			connectionManager.rollback();
+			throw e;
+		} finally {
+			connectionManager.closeConnection();
+		}
+	}
+	public int updateUser(int personalQuestionResult) {
+		ConnectionManager connectionManager = new ConnectionManager();
+		try {
+			Connection connection = connectionManager.getConnection();
+			UserDAO userDAO = new UserDAO(connection);
+			int result = userDAO.updateUser(personalQuestionResult);
+			connectionManager.commit();
+			return result;
+		} catch (RuntimeException e) {
+			connectionManager.rollback();
+			throw e;
+		} finally {
+			connectionManager.closeConnection();
+		}
+	}
 }
