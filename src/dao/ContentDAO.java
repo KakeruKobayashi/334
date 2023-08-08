09b939059da0c70eff8f6e76c42422b70944831c
KakeruKobayashi <kobayashi_k@mail.benesse.co.jp>
2023-08-08 13:38:09 +0900

    DAOのひな型作成

---
+++ src/dao/ContentDAO.java
@@ -0,0 +1,59 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Content;
+
+public class ContentDAO {
+	private Connection connection;
+
+	public ContentDAO(Connection connection) {
+		this.connection = connection;
+	}
+
+	public List<Content> selectContent(String courseName){
+		PreparedStatement statement = null;
+
+		try {
+			String sql = "";
+			statement = connection.prepareStatement(sql);
+
+			ResultSet result = statement.executeQuery();
+
+			List<Content> resultList = new ArrayList<Content>();
+
+			while(result.next()) {
+				Content content = new Content();
+				content.setContentName(result.getString(1));
+				content.setContentTime(result.getInt(2));
+				resultList.add(content);
+			}
+
+			return resultList;
+
+		}catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException("SQLの実行に失敗しました", e);
+
+		} finally {
+			try {
+				if (statement != null) {
+
+					statement.close();
+					System.out.println("ステートメントの解放に成功しました");
+
+				}
+			} catch (SQLException e) {
+
+				throw new RuntimeException("SQLの実行の後処理に失敗しました", e);
+
+			}
+		}
+	}
+}
---
+++ src/dao/UserDAO.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.User;
+
+public class UserDAO {
+	private Connection connection;
+
+	public UserDAO(Connection connection) {
+		this.connection = connection;
+	}
+
+	public int insertUser(User user) {
+		PreparedStatement statement = null;
+
+		try {
+			String sql = "";
+			statement = connection.prepareStatement(sql);
+
+			int result = statement.executeUpdate();
+			return result;
+
+		}catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException("SQLの実行に失敗しました", e);
+
+		} finally {
+			try {
+				if (statement != null) {
+
+					statement.close();
+					System.out.println("ステートメントの解放に成功しました");
+
+				}
+			} catch (SQLException e) {
+
+				throw new RuntimeException("SQLの実行の後処理に失敗しました", e);
+
+			}
+		}
+	}
+}
