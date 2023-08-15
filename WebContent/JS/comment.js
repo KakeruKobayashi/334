0da94cb13bde4b3b9c93f20f52465d305399bc50
Naohiro Murata <7419110@ed.tus.ac.jp>
2023-08-15 14:47:05 +0900

    css修正

--- WebContent/JS/comment.js
+++ WebContent/JS/comment.js
@@ -1,6 +1,15 @@
-/**
- *
- */
-document.getElementById("question-button").onclick = function() {
-    document.getElementById("text").innerHTML = TOEICComment;
-};
+const btn3 = document.getElementById('btn3');
+const mask = document.getElementById('mask');
+const modal = document.getElementById('modal');
+const modalContent = document.getElementById('modalContent'); // モーダルの内容を指定
+
+btn3.addEventListener('click', () => {
+  modalContent.innerText = "ボタン３がクリックされました！"; // ボタンクリック時に表示する内容
+  mask.classList.remove('hidden');
+  modal.classList.remove('hidden');
+});
+
+mask.addEventListener('click', () => {
+  mask.classList.add('hidden');
+  modal.classList.add('hidden');
+});
